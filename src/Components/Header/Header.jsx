41e78f106e505d7e1e025cc16b6de4404f00257b
penelope0314 <peyenhsieh003@gmail.com>
2025-09-06 00:22:43 +0800

    準備部署

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
             <div className='hidden md:flex font-sorts_mill_goudy fixed top-0 left-0 w-full justify-between items-center p-[1.5vw_4vw]'>
                 <h1><span className='text-[3.5vw]'>Penelope</span></h1>
                 <ul className='flex flex-row gap-[2vw] text-[1.7vw]'>
-                    <li><a href="#" className='nav_link'>About</a></li>
-                    <li><a href="#/portfolio" className='nav_link'>Portfolio</a></li>
+                    <li><Link to="" className='nav_link'>About</Link></li>
+                    <li><Link to="/portfolio" className='nav_link'>Portfolio</Link></li>
                 </ul>
             </div>
             <div className='mt-[6vw]'></div>
