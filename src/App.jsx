ce5e9394a72192640373bba2cf087392330051d2
penelope0314 <peyenhsieh003@gmail.com>
2025-09-05 23:29:37 +0800

    準備部署

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Rout} from 'react-router-dom';
 import './App.css'
 import Header from "./Components/Header/Header"
 import Portfolio from './Pages/portfolio'
@@ -8,13 +8,13 @@ import Photography from './Pages/photography'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/photography" element={<Photography />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
--- src/Pages/photography.jsx
+++ src/Pages/photography.jsx
@@ -5,7 +5,7 @@ export default function Photography() {
     return (
         <>
             <h2 className="text-center m-[7vw_0vw_3vw]"><span className="text-[4vw] md:text-[3vw] lg:text-[2.5vw] xl:text-[2vw]">OOTD</span></h2>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 p-[2vw_4vw] gap-[3vw] sm:gap-[4vw]">
+            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 p-[2vw_4vw] gap-[3vw] sm:gap-[2vw]">
                 {imageList.map((src, index) => (
                     <img key={index} src={src} alt={`圖片 ${index}`} className="rounded shadow-md" />
                 ))}
