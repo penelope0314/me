c01e4d918d8abf8523bd9f1c7e7de59925d20771
penelope0314 <peyenhsieh003@gmail.com>
2025-09-05 23:58:52 +0800

    準備部署

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header"
 import Portfolio from './Pages/portfolio'
 import Photography from './Pages/photography'
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+)
+
 function App() {
   return (
-    <BrowserRouter basename="/me">
+    <>
       <Header />
       <Routes>
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/photography" element={<Photography />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
