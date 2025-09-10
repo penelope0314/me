c3e0b6aa929f9017b1bbc6bd97f8cbf20ca7be01
penelope0314 <peyenhsieh003@gmail.com>
2025-09-10 16:49:26 +0800

    拿掉

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-const flowbitePlugin = require('flowbite/plugin');
 
 
 // https://vite.dev/config/
